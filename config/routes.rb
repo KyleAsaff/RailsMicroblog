657cb63807caa83ce23ac3b1851fa07ea24c110d
KyleAsaff <kyleasaff@gmail.com>
2015-03-25 20:51:30 -0300

    Added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
 
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  resources :microposts, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
