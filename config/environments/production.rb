e62cffa7f8d8d70917197bc19b938b3d1c8c4989
KyleAsaff <kyleasaff@gmail.com>
2015-03-27 23:41:19 -0300

    Changed microposts to chirps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
     config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'young-falls-3202.herokuapp.com'
+  host = 'tweetr.kylesbox.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
